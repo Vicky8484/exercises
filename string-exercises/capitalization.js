51d248cee408a42179d24c3dfbc8581f7b812707
“Victory <viok0001@stud.ek.dk>
2026-08-31 14:43:01 +0200

    capitalization

---
+++ string-exercises/capitalization.js
@@ -0,0 +1,10 @@
+// Use the 3caPitalization exercise as a basis for this exercise. Modify the code, so that only the first letter is upper case, and the rest is lower case.
+// Make sure that it can take names of any length!
+
+const sentence = "HEJ KROKODILLE!";
+
+const capLetter = sentence.substring(0,1).toUpperCase();
+const restSent = sentence.substring(1).toLowerCase();
+
+console.log(`${capLetter}${restSent}`); //samler det hele
+console.log(`${capLetter}EEEE${restSent}`); //smider noget ind imellem
