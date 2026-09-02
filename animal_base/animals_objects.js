9a00554df2e9a12e37594d3bf670e671669d5655
“Victory <viok0001@stud.ek.dk>
2026-09-02 22:56:39 +0200

    theme switcher rettet

--- animal_base/animals_objects.js
+++ animal_base/animals_objects.js
@@ -2,12 +2,27 @@
 
 window.addEventListener("DOMContentLoaded", start);
 
-const allAnimals = [];
+let allAnimals = [];
+
+const Animal = {
+name: "-default name-",
+desc: "-default desc-",
+type: "-default type-",
+age: 0
+}; //prototype object
+
 
 function start( ) {
     console.log("ready");
 
+    // Add event-listeners to filter and sort buttons
+    registerButtons();
     loadJSON();
+}
+
+function registerButtons(){
+   document.querySelectorAll("[data-action='filter']")
+   .forEach(button => button.addEventListener("click", selectFilter));
 }
 
 
@@ -22,8 +37,10 @@ function loadJSON() {
 
 function prepareObjects( jsonData ) {
     jsonData.forEach( jsonObject => {
-        // TODO: Create new object with cleaned data - and store that in the allAnimals array
-        
+        // Create new object with cleaned data - and store that in the allAnimals array
+        const animal = Object.create(Animal);
+
+        // Extract data from json Object
         const fullname = jsonObject.fullname;
 
         const firstSpace = fullname.indexOf(" ");
@@ -34,19 +51,83 @@ function prepareObjects( jsonData ) {
         const desc = fullname.substring(secondSpace + 1, lastSpace)
         const type = fullname.substring(lastSpace);
 
-        console.log(`name: _${name}_
-            desc: _${desc}_
-            type: _${type}_`
-        ) //_ checks for whitespace
+        // console.log(`name: _${name}_
+        //     desc: _${desc}_
+        //     type: _${type}_`
+        // ); //_ checks for whitespace
 
-        // TODO: MISSING CODE HERE !!!
+        // Put cleaned data into newly created Object
+
+        animal.name = name;
+        animal.desc = desc;
+        animal.type = type;
+
+        animal.age = jsonObject.age;
+
+        // Add the pbject to the global array
+        allAnimals.push(animal);
+    
     });
 
     displayList();
 }
 
-function displayList() {
-    // clear the list
+function selectFilter(event){
+    const filter = event.target.dataset.filter;
+    console.log(`User selected ${filter}`);
+    filterList(filter);
+}
+
+function filterList(filterBy){
+    let filteredList = allAnimals; 
+    if(filterBy === "cat"){
+    //create filtered list of only cats
+  filteredList = allAnimals.filter(isCat);
+    } else if(filterBy === "dog"){
+     //create filtered list of only dogs
+  filteredList = allAnimals.filter(isDog);
+    }
+
+    displayList(filteredList);
+}
+
+function isCat(animal){
+return animal.type === "cat";
+}
+
+function isDog(animal){
+return animal.type === "dog";
+}
+
+function sortList(sortBy){
+let sortedList = allAnimals;
+
+if (sortBy === "name"){
+   sortedList = sortedList.sort(sortByName);
+} else if (sortBy === "type"){
+sortedList = sortedList.sort(sortByType);
+}
+displayList(sortedList);
+}
+
+function sortByName(animalA, animalB){
+    if(animalA.name < animalB.name){
+      return -1;  
+    } else {
+        return 1;
+    }
+}
+
+function sortByType(animalA, animalB){
+    if(animalA.type < animalB.type){
+      return -1;  
+    } else {
+        return 1;
+    }
+}
+
+function displayList(animals) {
+ // clear the list
     document.querySelector("#list tbody").innerHTML = "";
 
     // build a new list
@@ -54,17 +135,21 @@ function displayList() {
 }
 
 function displayAnimal( animal ) {
+    console.log("displayAnimal called with:", animal);
+    
     // create clone
     const clone = document.querySelector("template#animal").content.cloneNode(true);
 
-    // set clone data
-    clone.querySelector("[data-field=name]").textContent = animal.name;
-    clone.querySelector("[data-field=desc]").textContent = animal.desc;
-    clone.querySelector("[data-field=type]").textContent = animal.type;
-    clone.querySelector("[data-field=age]").textContent = animal.age;
+        // set clone data
+        clone.querySelector("[data-field=name]").textContent = animal.name;
+        clone.querySelector("[data-field=desc]").textContent = animal.desc;
+        clone.querySelector("[data-field=type]").textContent = animal.type;
+        clone.querySelector("[data-field=age]").textContent = animal.age;
 
     // append clone to list
     document.querySelector("#list tbody").appendChild( clone );
 }
 
 
+  // append clone to list
+   
--- theme_switcher/switch.js
+++ theme_switcher/switch.js
@@ -1,24 +1,22 @@
-// Find dropdown-menuen i HTML.
-const themeSelect = document.querySelector("#theme-select");
-
-// Find body
+const themeSelect = document.querySelector("#theme-select"); // Find dropdown-menuen i HTML.
 const body = document.querySelector("body");
 
-body.dataset.theme = "light";
+body.dataset.theme = "light"; //er theme light til at starte med
 
-themeSelect.addEventListener("change", switchTheme);
+themeSelect.addEventListener("change", switchTheme); //eventlistener på options i selectoren
 
+//funktion der ændre tema
 function switchTheme(){
-    const selectedValue = themeSelect.value;
+    const selectedValue = themeSelect.value; //selectedValue er = den option der er valgt i selectoren
     
-    // Map dropdown values to theme names
+    // Mapper dropdown values til tema navne
     const themesList = {
         "dark": "dark",
         "light": "light",
         "hawaii": "hawaii"
     };
     
-    const theme = themesList[selectedValue];
+    const theme = themesList[selectedValue]; //finder temaet ud fra den valgte option og gemmer det i variablen theme
     
-    body.dataset.theme = theme ? theme : body.dataset.theme;
+    body.dataset.theme = theme ? theme : body.dataset.theme; //sætter data-theme på body til det valgte tema. Hvis temaet ikke findes, beholder den nuværende tema
 }
